f1a540c705e83807127ace4dc69b943396f5bf3c
charliebourland <chbo2810@colorado.edu>
2021-09-20 13:43:31 -0700

    files changes

--- functions_to_implement.cpp
+++ functions_to_implement.cpp
@@ -100,14 +100,23 @@ double Sign(double num);
 std::vector<int> AddN(std::vector<int>, int n);
 
 // adds n to each element of the vector
-std::vector<double> AddN(std::vector<double>, double n);
+std::vector<double> AddN(std::vector<double>, double n){
+        for(int i = 0; i<std::vector<int>.size(); i++){
+                std::vector<int>[i] += n;
+        }
+}
+
 
 // adds n to each element of the vector
 std::vector<std::string> AddN(std::vector<std::string>, std::string n);
 
 
 // subtracts n to each element of the vector
-std::vector<int> SubtractN(std::vector<int>, int n);
+std::vector<int> SubtractN(std::vector<int>, int n){
+	for(int i = 0; i<std::vector<int>.size(); i++){
+		std::vector<int>[i] -= n;
+	}
+}
 
 // subtracts n to each element of the vector
 std::vector<double> SubtractN(std::vector<double>, double n);
